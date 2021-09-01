479e9a265bd5215c8d36a4672b2dc91f04e08bbb
João Pedro Amaral Dias <joaopedrodiasamaral@gmail.com>
2021-09-01 16:13:30 -0300

    add LancamentoController

---
+++ src/controller/LancamentoController.ts
@@ -0,0 +1,11 @@
+import { getManager } from "typeorm";
+import { Lancamento } from "../entity/Lancamento";
+
+export class LancamentoController {
+
+  async salvar(lancamento: Lancamento) {
+    const lancamentoSalvo = await getManager().save(lancamento);
+    return lancamentoSalvo;
+  }
+
+}
